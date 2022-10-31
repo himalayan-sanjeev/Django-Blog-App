d8a3dd322d2c23ed90d0fc0606a83ae4e7c7fe09
himalayan-sanjeev <sanjeevpoudel111@gmail.com>
2022-10-31 21:13:17 +0545

    Add model comment

--- blog/models.py
+++ blog/models.py
@@ -20,4 +20,18 @@ class Post(models.Model):
 
     def __str__(self):
       return self.title
-  
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    # name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_post_comments')
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
