f5ccc6138bbd70c9d05e0d12eebe3746e7ec2905
himalayan-sanjeev <sanjeevpoudel111@gmail.com>
2022-10-31 21:27:58 +0545

    Add comment migration table

--- blog/models.py
+++ blog/models.py
@@ -23,8 +23,6 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
-    # name = models.CharField(max_length=80)
-    email = models.EmailField()
     body = models.TextField()
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_post_comments')
     created_on = models.DateTimeField(auto_now_add=True)
@@ -34,4 +32,4 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return 'Comment {} by {}'.format(self.body, self.name)
+        return 'Comment {}'.format(self.body)
